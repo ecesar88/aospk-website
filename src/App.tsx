714a7b6ff84af3642a052e3a1b900889ffe4402f
Erick <e.cesar88@gmail.com>
2021-07-19 19:07:13 -0700

    Add some css and fn return type on reportwebvitals

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import './App.css'
+import Home from './pages/home'
 
 const App: React.FC = (): JSX.Element => {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  )
 }
 
 export default App
